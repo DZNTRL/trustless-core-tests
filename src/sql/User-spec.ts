ac4c783ea465697eb34102faf9f57895be5bdd42
joshuaebowling <joshuaebowling@gmail.com>
2021-08-23 16:57:09 -0400

    JBOT-8 disabled Utils tests, will remove. add tests for create, checkunique, getPublicKey for User [wip]

--- src/sql/User-spec.ts
+++ src/sql/User-spec.ts
@@ -1,40 +1,66 @@
 import { expect } from "chai"
-import ProWebModels from "pro-web-models"
+import ProWebModels from "pro-web-core"
 import sinon from "sinon"
-import mysql from "mysql2"
+import mysql from "mysql2/promise"
 import { testData } from "./test-data"
 import config from "config"
 
 describe("User Repo Tests", function() {
     const db = config.get("db")
-    const stub = sinon.stub()
-    const fake = () => {
-        console.log("called")
-    }
     const _testData = testData("test");
     // stub(mysql, "createPool", fake)
+    this.afterAll(async () => {
+        const pool = await mysql.createPool(db)
+        pool.query("DELETE FROM Accounts")
+    })
     it("User class should exist on import", () => {
         expect(ProWebModels.User).to.be.an("function")
     })
     it("User class should instantiate with db connection info", async () => {
         const pool = await mysql.createPool(db)
         //@ts-ignore
-        expect(new ProWebModels.User(pool)).to.be.an("object")  
+        expect(new ProWebModels.User(pool)).to.be.instanceOf(ProWebModels.User)
     })
-    xit("User instance should return id > 0 when creating account", async () => {
-        const pool = await mysql.createPool(db)
-        //@ts-ignore
-        const instance = new ProWebModels.User(pool)
-        //@ts-ignore
-        const result = await instance.createUser(_testData.user[0].username, _testData.user[0].publicKey)
-        expect(result).to.be.greaterThan(0)
+    describe("User createUser tests", function() {
+        it("User instance should return id > 0 when creating account", async () => {
+            const pool = await mysql.createPool(db)
+            const instance = new ProWebModels.User(pool)
+            const result = await instance.createUser(_testData.user[0].username, _testData.user[0].publicKey)
+            expect(result.Data).to.be.greaterThan(0)
+        })    
     })
-    it("User instance should return false when username is unique", async () => {
-        const pool = await mysql.createPool(db)
-        //@ts-ignore
-        const instance = new ProWebModels.User(pool)
-        const result = await instance.checkUsernameUnique("TEST")
-        //@ts-ignore
-        expect(result.Data).to.equal(false)
+    describe("User checkUsernameUnique tests", function() {
+        it("User instance should return false when username is not unique", async () => {
+            const pool = await mysql.createPool(db)
+            const instance = new ProWebModels.User(pool)
+            const result = await instance.checkUsernameUnique(_testData.user[0].username)
+            expect(result.Data).to.equal(false)
+        })
+        it("User instance should return true when username is unique", async() => {
+            const pool = await mysql.createPool(db)
+            const instance = new ProWebModels.User(pool)
+            const result = await instance.checkUsernameUnique("ABCDEFGHIJKLACD")
+            expect(result.IsError).to.equal(false)
+            expect(result.Message).to.equal(ProWebModels.ResponseMessages.OK.toString())
+            expect(result.Data).to.equal(true)
+        })
+    })
+    describe("User getPublicKey tests", function() {
+        it("User instance should get publicKey for existing user", async() => {
+            const pool = await mysql.createPool(db)
+            const instance = new ProWebModels.User(pool)
+            const result = await instance.getPublicKey(_testData.user[0].username)
+            expect(result.IsError).to.equal(false)
+            expect(result.Message).to.equal(ProWebModels.ResponseMessages.OK.toString())
+            console.log(result.Data)
+        })
+        it("User instance should get publicKey for existing user", async() => {
+            const pool = await mysql.createPool(db)
+            const instance = new ProWebModels.User(pool)
+            const result = await instance.getPublicKey("ABCEEFDASFJ43")
+            expect(result.IsError).to.equal(false)
+            expect(result.Message).to.equal(ProWebModels.ResponseMessages.NotFound.toString())
+            console.log(result.Data)
+        })
     })
 })
